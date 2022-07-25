77e2e5c9ee1d8aed42a5655bfcb9012192c2011f
lvzhaohua <lvzhaohua@tripurx.com>
2022-07-25 17:59:20 +0800

    Initial Commit

---
+++ CIT634/CIT634App.swift
@@ -0,0 +1,17 @@
+//
+//  CIT634App.swift
+//  CIT634
+//
+//  Created by admin on 2022/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CIT634App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
